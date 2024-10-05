1658d1dc3a92a2867e67350c8978e66b425e3e7c
iKrazyCode <dodochavescadete13@gmail.com>
2024-10-05 10:02:32 -0300

    sistema de login quase pronto

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.custom_login, name='login'),
+    path('register/', views.custom_register, name='register'),
     path('logout/', views.custom_logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import get_user_model, authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import CustomUserForm
 
@@ -43,9 +44,41 @@ def custom_login(request):
 
 
 
+def custom_register(request):
+    if request.user.is_authenticated == True:
+        return redirect(reverse('home:home'))
+    
+    if request.method == 'POST':
+        form = CustomUserForm(data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = get_user_model()
+            try:
+                user.objects.get(username=username)
+                # Não pode criar, pois já existe
+                print('Usuário já existe')
+
+            except (user.DoesNotExist):
+                user_create = user.objects.create(username=username, password=password)
+                user_create.save()
+                login(request, user_create)
+                return redirect(reverse('home:home'))
+            
+        else:
+            print('Preencha o formulário corretamente')
+
+        return render(request, 'accounts/register.html')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+
 
 @login_required
 def custom_logout(request):
     logout(request)
     return redirect(reverse('accounts:login'))
+
 
